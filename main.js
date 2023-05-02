d0dcac2fafd3e385a095aab376e25b0e9e133424
Abdullah Ahmad <bdocoder@protonmail.com>
2023-05-02 11:02:28 +0300

    feat: update ui and implement logic

---
+++ main.js
@@ -0,0 +1,29 @@
+/** @type {HTMLSpanElement} */
+const span = document.querySelector(".loading-text");
+/** @type {HTMLImageElement} */
+const image = document.querySelector(".bg-image");
+const computed = getComputedStyle(image);
+
+const initialBlur = 30;
+
+const animateImage = () => {
+  image.classList.add("animate");
+};
+if (image.complete) animateImage();
+else {
+  image.onload = animateImage;
+}
+
+const updateText = () => {
+  const filterValue = computed.getPropertyValue("filter");
+  const blur = +filterValue.substring(5, filterValue.length - 3);
+  if (Number.isNaN(blur)) return;
+
+  const percentage = (blur * 100) / initialBlur;
+  span.textContent = Math.floor(100 - percentage) + "%";
+  span.style.opacity = percentage / 100;
+
+  requestAnimationFrame(updateText);
+};
+
+requestAnimationFrame(updateText);
